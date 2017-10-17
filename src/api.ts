506372a78be4cd7f4e91ff0c55390587a8ad9fe9
caasi Huang <caasi.igd@gmail.com>
2017-10-18 02:58:14 +0800

    Type selectors

--- src/api.ts
+++ src/api.ts
@@ -71,19 +71,73 @@ export namespace API {
     userid: string | null,
   }
 
-  // TODO:
-  //   type Selector
-  //     = FragmentSelector
-  //     | CSSSelector
-  //     | XPathSelector
-  //     | TextQuoteSelector
-  //     | TextPositionSelector
-  //     | DataPositionSelector
-  //     | SVGSelector
-  //     | RangeSelector
-  export interface Selector {
-    type: string
+  export interface BaseSelector {
+    type: string,
+    refinedBy?: Selector
   }
+
+  export interface FragmentSelector extends BaseSelector {
+    type: 'FragmentSelector',
+    value: string,
+    conformsTo?: string
+  }
+
+  export interface CSSSelector extends BaseSelector {
+    type: 'CSSSelector',
+    value: string
+  }
+
+  export interface XPathSelector extends BaseSelector {
+    type: 'XPathSelector',
+    value: string
+  }
+
+  export interface TextQuoteSelector extends BaseSelector {
+    type: 'TextQuoteSelector',
+    exact: string,
+    prefix?: string,
+    suffix?: string
+  }
+
+  export interface TextPositionSelector extends BaseSelector {
+    type: 'TextPositionSelector',
+    start: number,
+    end: number
+  }
+
+  export interface DataPositionSelector extends BaseSelector {
+    type: 'DataPositionSelector',
+    start: number,
+    end: number
+  }
+
+  export interface SVGSelector extends BaseSelector {
+    type: 'SVGSelector',
+    id?: string,
+    value?: string
+  }
+
+  export interface RangeSelector extends BaseSelector {
+    type: 'RangeSelector',
+    // startSelector is described in https://www.w3.org/TR/annotation-model/#range-selector
+    startSelector?: Selector,
+    // endSelector is described in https://www.w3.org/TR/annotation-model/#range-selector
+    endSelector?: Selector,
+    startContainer: string,
+    startOffset: number,
+    endContainer: string,
+    endOffset: number
+  }
+
+  export type Selector
+    = FragmentSelector
+    | CSSSelector
+    | XPathSelector
+    | TextQuoteSelector
+    | TextPositionSelector
+    | DataPositionSelector
+    | SVGSelector
+    | RangeSelector
 
   export interface AnnotationOption {
     group: string,
