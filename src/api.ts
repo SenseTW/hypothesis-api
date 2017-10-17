4d333edeb1529daac450deee3636c0a024389040
caasi Huang <caasi.igd@gmail.com>
2017-10-18 03:05:27 +0800

    Change coding style

--- src/api.ts
+++ src/api.ts
@@ -1,132 +1,132 @@
 export namespace API {
-  export type Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'HEAD'
+  export type Method = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'HEAD';
 
   export interface Link {
-    url: string,
-    method: Method,
-    desc: string
+    url: string;
+    method: Method;
+    desc: string;
   }
 
   export interface OtherLinks {
-    'account.settings'?: string,
-    'forget-password'?: string,
-    'groups.new'?: string,
-    help?: string,
-    'oauth.revoke'?: string,
-    'search.tag'?: string,
-    signup?: string,
-    user?: string
+    'account.settings'?: string;
+    'forget-password'?: string;
+    'groups.new'?: string;
+    help?: string;
+    'oauth.revoke'?: string;
+    'search.tag'?: string;
+    signup?: string;
+    user?: string;
   }
 
   export interface Resources {
-    message: string,
+    message: string;
     links: {
       profile: {
-        read: Link,
-        update: Link
-      },
-      search: Link,
+        read: Link;
+        update: Link;
+      };
+      search: Link;
       group: {
         member: {
-          delete: Link
-        }
-      },
+          delete: Link;
+        };
+      };
       annotation: {
-        hide: Link,
-        unhide: Link,
-        read: Link,
-        create: Link,
-        update: Link,
-        flag: Link,
-        delete: Link
-      },
-      links: Link
-    }
+        hide: Link;
+        unhide: Link;
+        read: Link;
+        create: Link;
+        update: Link;
+        flag: Link;
+        delete: Link;
+      };
+      links: Link;
+    };
   }
 
   export interface Group {
-    id: string,
-    name: string,
-    public: boolean
+    id: string;
+    name: string;
+    public: boolean;
   }
 
   export interface Profile {
-    authority: string,
+    authority: string;
     features: {
-      defer_realtime_updates: boolean,
-      flag_action: boolean,
-      orphans_tab: boolean,
-      client_oauth: boolean,
-      overlay_highlighter: boolean,
-      total_shared_annotations: boolean,
-      api_render_user_info: boolean,
-      embed_cachebuster: boolean,
-      filter_highlights: boolean,
-      search_for_doi: boolean
-    },
-    groups: Group[],
+      defer_realtime_updates: boolean;
+      flag_action: boolean;
+      orphans_tab: boolean;
+      client_oauth: boolean;
+      overlay_highlighter: boolean;
+      total_shared_annotations: boolean;
+      api_render_user_info: boolean;
+      embed_cachebuster: boolean;
+      filter_highlights: boolean;
+      search_for_doi: boolean;
+    };
+    groups: Group[];
     preferences: {
       [key: string]: boolean
-    },
-    userid: string | null,
+    };
+    userid: string | null;
   }
 
   export interface BaseSelector {
-    type: string,
-    refinedBy?: Selector
+    type: string;
+    refinedBy?: Selector;
   }
 
   export interface FragmentSelector extends BaseSelector {
-    type: 'FragmentSelector',
-    value: string,
-    conformsTo?: string
+    type: 'FragmentSelector';
+    value: string;
+    conformsTo?: string;
   }
 
   export interface CSSSelector extends BaseSelector {
-    type: 'CSSSelector',
-    value: string
+    type: 'CSSSelector';
+    value: string;
   }
 
   export interface XPathSelector extends BaseSelector {
-    type: 'XPathSelector',
-    value: string
+    type: 'XPathSelector';
+    value: string;
   }
 
   export interface TextQuoteSelector extends BaseSelector {
-    type: 'TextQuoteSelector',
-    exact: string,
-    prefix?: string,
-    suffix?: string
+    type: 'TextQuoteSelector';
+    exact: string;
+    prefix?: string;
+    suffix?: string;
   }
 
   export interface TextPositionSelector extends BaseSelector {
-    type: 'TextPositionSelector',
-    start: number,
-    end: number
+    type: 'TextPositionSelector';
+    start: number;
+    end: number;
   }
 
   export interface DataPositionSelector extends BaseSelector {
-    type: 'DataPositionSelector',
-    start: number,
-    end: number
+    type: 'DataPositionSelector';
+    start: number;
+    end: number;
   }
 
   export interface SVGSelector extends BaseSelector {
-    type: 'SVGSelector',
-    id?: string,
-    value?: string
+    type: 'SVGSelector';
+    id?: string;
+    value?: string;
   }
 
   export interface RangeSelector extends BaseSelector {
-    type: 'RangeSelector',
+    type: 'RangeSelector';
     // startSelector is described in https://www.w3.org/TR/annotation-model/#range-selector
-    startSelector?: Selector,
+    startSelector?: Selector;
     // endSelector is described in https://www.w3.org/TR/annotation-model/#range-selector
-    endSelector?: Selector,
-    startContainer: string,
-    startOffset: number,
-    endContainer: string,
-    endOffset: number
+    endSelector?: Selector;
+    startContainer: string;
+    startOffset: number;
+    endContainer: string;
+    endOffset: number;
   }
 
   export type Selector
@@ -140,38 +140,38 @@ export namespace API {
     | RangeSelector
 
   export interface AnnotationOption {
-    group: string,
+    group: string;
     permissions: {
-      read: string[],
-      admin: string[],
-      update: string[],
-      delete: string[]
-    },
-    references?: string[],
-    tags: string[],
+      read: string[];
+      admin: string[];
+      update: string[];
+      delete: string[];
+    };
+    references?: string[];
+    tags: string[];
     target: {
-      source: string,
-      selector: Selector[]
-    }[],
-    text: string,
-    uri: string
+      source: string;
+      selector: Selector[];
+    }[];
+    text: string;
+    uri: string;
   }
 
   export interface Annotation extends AnnotationOption {
-    id: string,
-    created: string,
-    updated: string,
-    flagged: boolean,
-    user: string,
-    hidden: boolean,
+    id: string;
+    created: string;
+    updated: string;
+    flagged: boolean;
+    user: string;
+    hidden: boolean;
     document: {
-      title?: string[]
-    },
+      title?: string[];
+    };
     links: {
-      json: string,
-      html: string,
-      incontext: string
-    }
+      json: string;
+      html: string;
+      incontext: string;
+    };
   }
 
   export interface SearchOption {
@@ -179,16 +179,16 @@ export namespace API {
     offset?: number, // 0
     sort?: string, // 'updated'
     order?: 'desc' | 'asc', // 'desc'
-    uri?: string,
-    url?: string,
-    user?: string,
-    group?: string,
-    tag?: string,
-    any?: string
+    uri?: string;
+    url?: string;
+    user?: string;
+    group?: string;
+    tag?: string;
+    any?: string;
   }
 
   export interface SearchResult<T> {
-    rows: T[],
-    total: number
+    rows: T[];
+    total: number;
   }
 }
